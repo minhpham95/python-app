17af4a9890195500e8274b07c3fba8be2d190bb7
minhpham95 <chauchau310897@gmail.com>
2025-07-13 22:29:03 +0700

    add github runner self hosted

--- src/app.py
+++ src/app.py
@@ -32,4 +32,4 @@ if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(host="0.0.0.0") 
+    app.run(host="0.0.0.0")
